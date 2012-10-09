86f72aa80430f2eb1bd8cfa253565aa85a435235
Wakaba <wakabatan@hatena.ne.jp>
2012-10-09 20:31:38 +0900

    config template

---
+++ config/service.in/testd/bin.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+exec 2>&1
+export MYSQL_DSNS_JSON=@@INSTANCECONFIG@@/dsns.json
+export KARASUMA_CONFIG_JSON=@@INSTANCECONFIG@@/@@INSTANCENAME@@.json
+export KARASUMA_CONFIG_FILE_DIR_NAME=@@LOCAL@@/keys
+exec setuidgid @@USER@@ @@ROOT@@/perl @@ROOT@@/bin/runner.pl
