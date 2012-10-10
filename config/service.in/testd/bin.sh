c949efe08bd64c07d52c18600ddc00211d138567
Wakaba <wakabatan@hatena.ne.jp>
2012-10-10 20:01:02 +0900

    debug

--- config/service.in/testd/bin.sh
+++ config/service.in/testd/bin.sh
@@ -3,4 +3,5 @@ exec 2>&1
 export MYSQL_DSNS_JSON=@@INSTANCECONFIG@@/dsns.json
 export KARASUMA_CONFIG_JSON=@@INSTANCECONFIG@@/@@INSTANCENAME@@.json
 export KARASUMA_CONFIG_FILE_DIR_NAME=@@LOCAL@@/keys
+export WEBUA_DEBUG=2
 exec setuidgid @@USER@@ @@ROOT@@/perl @@ROOT@@/bin/runner.pl
